ad434f24a39c6cb0eb431e2746bf096df54e5487
root <GrupoTresG3@gmail.com>
2017-10-26 08:25:43 +0000

    Version 1.0 Wordpress y Owncloud OK

---
+++ html/owncloud/config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocqj15wf5g6u',
+  'passwordsalt' => 'TnIGrLqkEppJMstIBeDzxTxhIKv1ja',
+  'secret' => 'MprF0DW5I/n0x4TviQR0PsmLGolBDNVTioevA/waPaFlA0A+',
+  'trusted_domains' => 
+  array (
+    0 => '10.9.53.106',
+  ),
+  'datadirectory' => '/var/www/html/owncloud/data',
+  'overwrite.cli.url' => 'http://10.9.53.106/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '10.0.3.3',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'ownclouduser',
+  'dbpassword' => 'ownclouduser',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
